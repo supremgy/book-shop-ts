863174e00f279150427248c47f6ebf2956baaaa4
supremgy <supremgy@icloud.com>
2024-01-17 21:07:20 +0900

    userController setting

--- server/dist/app.js
+++ server/dist/app.js
@@ -5,12 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const users_1 = __importDefault(require("./routes/users"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.get('/', (req, res) => {
-    res.send('hellow world');
-});
 app.listen(process.env.PORT, () => {
     console.log('server is running');
 });
+app.use('/users', users_1.default);
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,12 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
 import dotenv from 'dotenv';
+import userRouter from './routes/users';
 dotenv.config();
+
 const app: Express = express();
 app.use(express.json());
-app.get('/', (req: Request, res: Response) => {
-  res.send('hellow world');
-});
 app.listen(process.env.PORT, () => {
   console.log('server is running');
 });
+
+app.use('/users', userRouter);
