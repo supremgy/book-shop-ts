f1b38ceb75f75254ecab683b9604433d9c9969cd
supremgy <supremgy@icloud.com>
2024-01-17 19:31:25 +0900

    dotenv 추가

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,11 @@
 import express, { Express, Request, Response, NextFunction } from 'express';
-
+import dotenv from 'dotenv';
+dotenv.config();
 const app: Express = express();
 app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.send('hellow world');
 });
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
   console.log('server is running');
 });
